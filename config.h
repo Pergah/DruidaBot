dd65452a064ed79ccebd709acf609e4ce8da2473
Pergah <119632014+Pergah@users.noreply.github.com>
2024-10-11 17:00:26 -0300

    Add files via upload

--- config.h
+++ config.h
@@ -1,5 +1,12 @@
 
-/* Druida BOT 1.618
+/* Druida BOT 1.619
+
+El programa es capaz de controlar 4 reles independientes. 
+Pueden funcionar en modo Manual, o Automatico.
+Rele 1: Sube parametro (Hum, Temp, DPV, TempA)
+Rele 2: Baja parametro (Hum, Temp, DPV, TempA) 
+Rele 3: Timer diario (Ej: Luz) (Hora encendido / Hora Apagado)
+Rele 4: Timer diario + semanal (Ej: Riego) (Hora Enc / Hora Apag) + (DiaRiego / DiaNoRiego)
 
 Conexion de sensores: 
 
@@ -9,6 +16,23 @@ Sensor IR Receptor -> 14
 Temperatura y Humedad ambiente -> 19
 Sensor PH -> 34
 
+Chenge Log:
+
+* La red WiFi se puede cambiar desde el serial
+* El chat ID se puede cambiar desde el serial
+* Solucionado, no guardaba bien horarios R3 y R4
+* Se agrego la funcion "/resetDruidaBot" para reiniciar a distancia el dispositivo
+* Se mejoro sistema anti caida de internet (antes se bugeaba al caerse el internet)
+* Envia datos a una hoja de calculo de google
+* Se agrego la funcion de medir PH
+* Se agrego funcion de Medir temperatura de Agua
+* Se agrego funcion para enviar señal IR en el R2, se cargan los valores manualmente.
+* Se optimizo la parte del codigo donde se encienden y apagan los Rele.
+* Se agrego la funcion de "clonar" la señal IR con un sensor Receptor. (carga automatica)
+* Se arreglo la logica de R3 y R4, fallaba cuando HoraEnc > HoraApag
+* Se agrego la funcion de encender los reles por X segundos en modo Manual (/R1onTime)
+* Se agrego un nuevo Rele, llamado "Rele 2 IR", en vez de encender un Rele, manda una señal por infrarojo. (Especial, aire acondicionado).
+* Falta modificar logica del R3 para que pueda accionar o no, en funcion del sensor de HumedadSuelo
 */
 
 #include "esp_system.h"
@@ -24,14 +48,15 @@ Sensor PH -> 34
 #include <Time.h>
 //#include <TimeAlarms.h>
 #include <HTTPClient.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
+//#include <OneWire.h>
+//#include <DallasTemperature.h>
 #include <Arduino.h>
 #include <IRrecv.h>
 #include <IRremoteESP8266.h>
 #include <IRutils.h>
 #include <IRsend.h>
 #include <Adafruit_AHTX0.h>
+#include <WebServer.h>
 
 
 #define H 1
@@ -44,19 +69,20 @@ Sensor PH -> 34
 #define CONFIG 3
 #define STATUS 4
 #define AUTOINT 5
+#define TIMER 6
 
 // Aca se muestra como van conectados los componentes
 
-#define sensorTempAgua 5  //Sensor DS
-#define sensorIRpin 12    //Sensor Emisor IR
-#define sensorHS 13
-#define sensorIRreceptor 14
-#define RELAY4 15
-#define RELAY3 16
+//#define sensorTempAgua 5  //Sensor DS
+#define sensorIRpin 32    //Sensor Emisor IR
+//#define sensorHS 2 //humedad Suelo
+#define sensorIRreceptor 33
+#define RELAY4 19
+#define RELAY3 18
 #define RELAY2 17
-#define RELAY1 18
-#define sensor1PIN 19  //Temperatura y humedad Ambiente
-#define SensorPin 34   //PH
+#define RELAY1 16
+//#define sensorHS 34  //humedad Suelo
+//#define SensorPin 35   //PH
 
 #define Offset 0.00  // deviation compensate
 #define samplingInterval 20
@@ -76,9 +102,18 @@ uint16_t IRsignalLength = 0;
 IRsend irsend(sensorIRpin);
 
 //const String botToken = "6920896340:AAEdvJl1v67McffACbdNXLhjMe00f_ji_ag"; //DRUIDA UNO (caba y roge)
-//const String botToken = "6867697701:AAHtaJ4YC3dDtk1RuFWD-_f72S5MYvlCV4w"; //DRUIDA DOS (rasta)
+//const String botToken = "6867697701:AAHtaJ4YC3dDtk1RuFWD-_f72S5MYvlCV4w"; //DRUIDA DOS (matheu 2)
 //const String botToken = "7273841170:AAHxWF33cIDcIoxgBm3x9tzn9ISJIKoy7X8"; //DRUIDA TRES (brai e ivana)
-const String botToken = "7314697588:AAGJdgljHPSb47EWcfYUR1Rs-7ia0_domok";  //DRUIDA CUATRO (matheu)
+const String botToken = "7357647743:AAFPD1Tc099-2o-E2-Ph7SZluzwHubrl700";  //DRUIDA CINCO
+//const String botToken = "7314697588:AAGJdgljHPSb47EWcfYUR1Rs-7ia0_domok"; //DRUIDA CUATRO (matheu)
+
+
+//const char* ssid_AP = "Druida Cuatro";  
+const char* ssid_AP = "Druida Cinco";           // Nombre de la red AP creada por el ESP32
+const char* password_AP = "12345678";          // Contraseña de la red AP
+
+//String scriptId = "AKfycbwXhUu15DVEI4b1BDf8Y8Up_qKIXDUvfWgHLKppNL6rUMOnfiQRDfxGXtCt3_n0NXt_Nw"; //Druida UNO (Caba)
+String scriptId = "AKfycbwUlj-gk1NNDHwxxebIqH7vS0N8qbu9LZydo4QeyAwmULEQ8JcSGNt8RRxRLdoIRRTA";  //Druida TRES (Matheu)
 
 const unsigned long BOT_MTBS = 1000;
 const int MAX_STRING_LENGTH = 32;
@@ -86,19 +121,18 @@ unsigned long bot_lasttime;
 const unsigned long wifiCheckInterval = 600000;  //WiFi CheckStatus cada 10 minutos
 unsigned long previousMillis = 0;
 
-const int oneWireBus = sensorTempAgua;
+//const int oneWireBus = sensorTempAgua;
 
 WiFiClientSecure secured_client;
 UniversalTelegramBot bot(botToken, secured_client);
-
 
 //DHT dht(sensor1PIN, DHT11);
 //DHT dht(sensor1PIN, DHT22);
 
 Adafruit_AHTX0 aht;
 
-OneWire oneWire(oneWireBus);
-DallasTemperature sensors(&oneWire);
+//OneWire oneWire(oneWireBus);
+//DallasTemperature sensors(&oneWire);
 
 RTC_DS3231 rtc;
 
@@ -106,8 +140,7 @@ String chat_id = "";
 String ssid = "";
 String password = "";
 
-//String scriptId = "AKfycbwXhUu15DVEI4b1BDf8Y8Up_qKIXDUvfWgHLKppNL6rUMOnfiQRDfxGXtCt3_n0NXt_Nw"; //Druida UNO (Caba)
-String scriptId = "AKfycbwUlj-gk1NNDHwxxebIqH7vS0N8qbu9LZydo4QeyAwmULEQ8JcSGNt8RRxRLdoIRRTA";  //Druida TRES (Matheu)
+
 
 int conPW = 1;
 int reset = 0;
@@ -116,19 +149,30 @@ byte modoR1 = 0;
 float minR1 = 0;
 float maxR1 = 0;
 byte paramR1 = 0;
+byte timeOnR1 = 0;
+byte timeOffR1 = 0;
+byte horaOnR1 = 0;
+byte minOnR1 = 0;
+byte horaOffR1 = 0;
+byte minOffR1 = 0;
 
 byte modoR2 = 0;
 float minR2 = 0;
 float maxR2 = 0;
 byte paramR2 = 0;
 
+byte modoR2ir = 0;
+float minR2ir = 0;
+float maxR2ir = 0;
+byte paramR2ir = 0;
+
 byte modoR3 = 0;
-int timeOnR3 = 0;
-int timeOffR3 = 0;
 int minR3 = 0;
 int maxR3 = 0;
 byte paramR3 = HS;
 byte diasRiego[7];
+int timeOnR3 = 0;
+int timeOffR3 = 0;
 
 byte modoR4 = 0;
 int timeOnR4 = 0;
@@ -138,16 +182,19 @@ byte modoMenu = -1;
 
 int R1config = -1;
 int R2config = -1;
+int R2irconfig = -1;
 int R3config = -1;
 int R4config = -1;
 
 byte estadoR1 = 0;
 byte estadoR2 = 0;
+byte estadoR2ir = 0;
 byte estadoR3 = 0;
 byte estadoR4 = 0;
 
 bool R1estado = HIGH;
 bool R2estado = HIGH;
+bool R2irestado = HIGH;
 bool R3estado = HIGH;
 bool R4estado = HIGH;
 
@@ -170,16 +217,28 @@ float minTemp = 999;
 
 int lastHourSent = -1;
 
-float PHval, PHvolt;
+//float PHval, PHvolt;
 byte estadoRTC = 0;
 
 int tiempoR1 = 0;
 int tiempoR2 = 0;
+int tiempoR2ir = 0;
 int tiempoR3 = 0;
 int tiempoR4 = 0; 
 bool esperandoTiempoR1 = false;
 bool esperandoTiempoR2 = false;
+bool esperandoTiempoR2ir = false;
 bool esperandoTiempoR3 = false;
 bool esperandoTiempoR4 = false;
+
+//int humedadSuelo;
+//float temperatureC;
+//static float pHValue, voltage;
+//int humedadMinima = 3400; // Valor en aire (seco)
+//int humedadMaxima = 1600; // Valor en agua (húmedo)
+
+
+
+WebServer server(80);
 
 
--- handleNewMessage.ino
+++ handleNewMessage.ino
@@ -11,6 +11,8 @@ void handleNewMessages(int numNewMessages) {
     if (from_name == "")
       from_name = "Guest";
 
+  //  int commandCode = getCommandCode(text);
+
 
     //MENU PRINCIPAL
 
@@ -37,9 +39,11 @@ void handleNewMessages(int numNewMessages) {
       String modoManu = "MODO MANUAL \n";
       modoManu += "/R1on - /R1off - /R1onTime\n";
       modoManu += "/R2on - /R2off - /R2onTime\n";
+      modoManu += "/R2iron - /R2iroff - /R2ironTime\n";
       modoManu += "/R3on - /R3off - /R3onTime\n";
       modoManu += "/R4on - /R4off - /R4onTime\n";
       modoManu += "/controlRemoto \n";
+      modoManu += "/manual \n";
       bot.sendMessage(chat_id, modoManu, "Markdown");
       delay(500);
 
@@ -74,10 +78,27 @@ void handleNewMessages(int numNewMessages) {
         delay(500);
       }
 
+        if (text == "/R2iron") {
+
+        modoR2ir = MANUAL;
+        estadoR2ir = 1;
+        bot.sendMessage(chat_id, "Rele 2 (IR) is ON", "");
+        Guardado_General();
+        delay(500);
+      }
+
       if (text == "/R2off") {
         modoR2 = MANUAL;
         estadoR2 = 0;
         bot.sendMessage(chat_id, "Rele 2 is OFF", "");
+        Guardado_General();
+        delay(500);
+      }
+
+            if (text == "/R2iroff") {
+        modoR2ir = MANUAL;
+        estadoR2ir = 0;
+        bot.sendMessage(chat_id, "Rele 2 (IR) is OFF", "");
         Guardado_General();
         delay(500);
       }
@@ -137,6 +158,12 @@ void handleNewMessages(int numNewMessages) {
         delay(500);
       }
 
+      if (text == "/R2ironTime") {
+        esperandoTiempoR2ir = true; // Establece que estamos esperando el tiempo de encendido para R2
+        bot.sendMessage(chat_id, "¿Por cuánto tiempo (en segundos) quieres encender el Rele 2 (IR)?", "");
+        delay(500);
+      }
+
       if (text == "/R3onTime") {
         esperandoTiempoR3 = true; // Establece que estamos esperando el tiempo de encendido para R3
         bot.sendMessage(chat_id, "¿Por cuánto tiempo (en segundos) quieres encender el Rele 3?", "");
@@ -166,6 +193,17 @@ void handleNewMessages(int numNewMessages) {
         if (tiempoR2 > 0) { // Si el valor ingresado es válido
           esperandoTiempoR2 = false; // Resetea la variable
           encenderRele2PorTiempo(tiempoR2); // Enciende el relé 2 por el tiempo indicado
+        } else {
+          bot.sendMessage(chat_id, "Por favor ingresa un valor válido en segundos.", "");
+        }
+        delay(500);
+      }
+
+            if (esperandoTiempoR2ir && text != "/R2ironTime") {
+        tiempoR2ir = text.toInt(); // Convierte el texto ingresado en un número entero
+        if (tiempoR2ir > 0) { // Si el valor ingresado es válido
+          esperandoTiempoR2ir = false; // Resetea la variable
+          encenderRele2irPorTiempo(tiempoR2ir); // Enciende el relé 2 por el tiempo indicado
         } else {
           bot.sendMessage(chat_id, "Por favor ingresa un valor válido en segundos.", "");
         }
@@ -202,7 +240,9 @@ void handleNewMessages(int numNewMessages) {
       modoMenu = AUTO;
       String modoManu = "MODO AUTOMATICO: \n";
       modoManu += "/R1auto\n";
+      modoManu += "/R1timer\n";
       modoManu += "/R2auto\n";
+      modoManu += "/R2irauto\n";
       modoManu += "/R3auto\n";
       modoManu += "/R3autoParam\n";
       modoManu += "/R4auto\n";
@@ -218,9 +258,24 @@ void handleNewMessages(int numNewMessages) {
         delay(500);
       }
 
+        if (text == "/R1timer") {
+
+        modoR1 = TIMER;
+        bot.sendMessage(chat_id, "Rele 1 Automatico (TIMER)", "");
+        Guardado_General();
+        delay(500);
+      }
+
       if (text == "/R2auto") {
         modoR2 = AUTO;
         bot.sendMessage(chat_id, "Rele 2 Automatico", "");
+        Guardado_General();
+        delay(500);
+      }
+
+            if (text == "/R2irauto") {
+        modoR2ir = AUTO;
+        bot.sendMessage(chat_id, "Rele 2 (IR) Automatico", "");
         Guardado_General();
         delay(500);
       }
@@ -262,9 +317,16 @@ void handleNewMessages(int numNewMessages) {
     modoConf += "/minR1config\n";
     modoConf += "/maxR1config\n";
     modoConf += "/paramR1config\n";
+    modoConf += "/horaOnR1config\n";
+    modoConf += "/minOnR1config\n";
+    modoConf += "/horaOffR1config\n";
+    modoConf += "/minOffR1config\n";
     modoConf += "/minR2config\n";
     modoConf += "/maxR2config\n";
     modoConf += "/paramR2config\n";
+    modoConf += "/minR2irconfig\n";
+    modoConf += "/maxR2irconfig\n";
+    modoConf += "/paramR2irconfig\n";
     modoConf += "/horaOnR3config\n";
     modoConf += "/minOnR3config\n";
     modoConf += "/horaOffR3config\n";
@@ -346,6 +408,82 @@ void handleNewMessages(int numNewMessages) {
       }
     }
 
+        if (text == "/horaOnR1config") {
+      modoR1 = CONFIG;
+      modoMenu = CONFIG;
+      R1config = 4;
+      bot.sendMessage(chat_id, "Ingrese parametro Hora On R1: ");
+    }
+    if (R1config == 4) {
+      horaOnR1 = text.toInt();
+
+      if (horaOnR1 > 0) {
+        Serial.print("Hora On R1: ");
+        Serial.println(horaOnR1);
+        bot.sendMessage(chat_id, "Valor horaOnR1 guardado");
+        bot.sendMessage(chat_id, modoConf, "Markdown");
+        Guardado_General();
+        R1config = 0;
+      }
+    }
+
+      if (text == "/horaOffR1config") {
+      modoR1 = CONFIG;
+      modoMenu = CONFIG;
+      R1config = 5;
+      bot.sendMessage(chat_id, "Ingrese Hora Off R1:");
+    }
+    if (R1config == 5) {
+      horaOffR1 = text.toInt();
+
+      if (horaOffR1 > 0) {
+        Serial.print("Hora Off R1: ");
+        Serial.println(horaOffR1);
+        bot.sendMessage(chat_id, "Valor horaOffR1 guardado");
+        bot.sendMessage(chat_id, modoConf, "Markdown");
+        Guardado_General();
+        R1config = 0;
+      }
+    }
+
+          if (text == "/minOnR1config") {
+      modoR1 = CONFIG;
+      modoMenu = CONFIG;
+      R1config = 6;
+      bot.sendMessage(chat_id, "Ingrese Min On R1:");
+    }
+    if (R1config == 6) {
+      minOnR1 = text.toInt();
+
+      if (minOnR1 > 0) {
+        Serial.print("Min On R1: ");
+        Serial.println(minOnR1);
+        bot.sendMessage(chat_id, "Valor minOnR1 guardado");
+        bot.sendMessage(chat_id, modoConf, "Markdown");
+        Guardado_General();
+        R1config = 0;
+      }
+    }
+
+              if (text == "/minOffR1config") {
+      modoR1 = CONFIG;
+      modoMenu = CONFIG;
+      R1config = 7;
+      bot.sendMessage(chat_id, "Ingrese Min Off R1:");
+    }
+    if (R1config == 7) {
+      minOffR1 = text.toInt();
+
+      if (minOffR1 > 0) {
+        Serial.print("Min Off R1: ");
+        Serial.println(minOffR1);
+        bot.sendMessage(chat_id, "Valor minOffR1 guardado");
+        bot.sendMessage(chat_id, modoConf, "Markdown");
+        Guardado_General();
+        R1config = 0;
+      }
+    }
+
     /// R2
 
     if (text == "/minR2config") {
@@ -387,9 +525,7 @@ void handleNewMessages(int numNewMessages) {
       }
     }
 
-    ///
-
-    if (text == "/paramR2config") {
+        if (text == "/paramR2config") {
       modoR2 = CONFIG;
       modoMenu = CONFIG;
       R2config = 3;
@@ -405,6 +541,68 @@ void handleNewMessages(int numNewMessages) {
         bot.sendMessage(chat_id, modoConf, "Markdown");
         Guardado_General();
         R2config = 0;
+      }
+    }
+
+
+    // R2 IR
+
+    if (text == "/minR2irconfig") {
+      modoR2ir = CONFIG;
+      modoMenu = CONFIG;
+      R2irconfig = 1;
+      bot.sendMessage(chat_id, "Ingrese valor Min R2 (IR): ");
+    }
+    if (R2irconfig == 1) {
+      minR2ir = text.toFloat();
+
+      if (minR2ir > 0) {
+        Serial.print("Valor min R2 (IR): ");
+        Serial.println(minR2ir);
+        bot.sendMessage(chat_id, "Valor min R2 (IR) guardado");
+        bot.sendMessage(chat_id, modoConf, "Markdown");
+        Guardado_General();
+        R2irconfig = 0;
+      }
+    }
+
+    if (text == "/maxR2irconfig") {
+      modoR2ir = CONFIG;
+      modoMenu = CONFIG;
+      R2irconfig = 2;
+      bot.sendMessage(chat_id, "Ingrese valor Max R2 (IR): ");
+    }
+    if (R2irconfig == 2) {
+      maxR2ir = text.toFloat();
+
+      if (maxR2ir > 0) {
+        Serial.print("Valor max R2 (IR): ");
+        Serial.println(maxR2ir);
+        bot.sendMessage(chat_id, "Valor max R2 (IR) guardado");
+        bot.sendMessage(chat_id, modoConf, "Markdown");
+        Guardado_General();
+        R2irconfig = 0;
+      }
+    }
+
+    ///
+
+    if (text == "/paramR2irconfig") {
+      modoR2ir = CONFIG;
+      modoMenu = CONFIG;
+      R2irconfig = 3;
+      bot.sendMessage(chat_id, "Ingrese parametro IR: \n1- Humedad.\n2- Temperatura.\n3- DPV.\n4- Temp Agua.");
+    }
+    if (R2irconfig == 3) {
+      paramR2ir = text.toInt();
+
+      if (paramR2ir > 0) {
+        Serial.print("Param R2 (IR): ");
+        Serial.println(paramR2ir);
+        bot.sendMessage(chat_id, "Valor param R2 (IR) guardado");
+        bot.sendMessage(chat_id, modoConf, "Markdown");
+        Guardado_General();
+        R2irconfig = 0;
       }
     }
 
@@ -737,15 +935,21 @@ void handleNewMessages(int numNewMessages) {
       infoConfig += "minR1: " + String(minR1) + ".\n";
       infoConfig += "maxR1: " + String(maxR1) + ".\n";
       infoConfig += "paramR1: " + String(paramR1) + ".\n";
-      infoConfig += "modoR1: " + String(modoR1) + ".\n";
+      infoConfig += "Hora de encendido: " + String(horaOnR1) + ":" + String(minOnR1) + "\n";
+      infoConfig += "Hora de apagado: " + String(horaOffR1) + ":" + String(minOffR1) + "\n";      
+      infoConfig += "modoR1: " + String(modoR1) + ".\n\n";
       infoConfig += "Rele 2: \n";
       infoConfig += "minR2: " + String(minR2) + ".\n";
       infoConfig += "maxR2: " + String(maxR2) + ".\n";
-      infoConfig += "modoR2: " + String(modoR2) + ".\n";
+      infoConfig += "modoR2: " + String(modoR2) + ".\n\n";
+      infoConfig += "Rele 2 (IR): \n";
+      infoConfig += "minR2ir: " + String(minR2ir) + ".\n";
+      infoConfig += "maxR2ir: " + String(maxR2ir) + ".\n";
+      infoConfig += "modoR2ir: " + String(modoR2ir) + ".\n\n";
       infoConfig += "Rele 3: \n";
       infoConfig += "Hora de encendido: " + String(horaOnR3) + ":" + String(minOnR3) + "\n";
       infoConfig += "Hora de apagado: " + String(horaOffR3) + ":" + String(minOffR3) + "\n";
-      infoConfig += "modoR3: " + String(modoR3) + ".\n";
+      infoConfig += "modoR3: " + String(modoR3) + ".\n\n";
       infoConfig += "Rele 4: \n";
       infoConfig += "Hora de encendido: " + String(horaOnR4) + ":" + String(minOnR4) + "\n";
       infoConfig += "Hora de apagado: " + String(horaOffR4) + ":" + String(minOffR4) + "\n";
@@ -766,9 +970,20 @@ void handleNewMessages(int numNewMessages) {
       float temperature = (temp.temperature);
       float humedad = (humidity.relative_humidity);
 
+      //int humedadS = analogRead(sensorHS); // Lee el valor analógico del sensor
+      //int humedadSuelo = map(humedadS, 0, 4095, 0, 100);
 
-      sensors.requestTemperatures();
-      float temperatureC = sensors.getTempCByIndex(0);
+      // Mapear el valor del sensor al rango de 0 a 100
+     // int sensorValue = analogRead(sensorHS);
+  //int humedadS = map(sensorValue, humedadMinima, humedadMaxima, 0, 100);
+
+  // Limitar el valor de humedad a 0 y 100
+  //if (humedadS < 0) humedadS = 0;
+  //if (humedadS > 100) humedadS = 100;
+
+
+     // sensors.requestTemperatures();
+    //  float temperatureC = sensors.getTempCByIndex(0);
 
       DateTime now = rtc.now();
       int horaBot = now.hour();
@@ -786,9 +1001,11 @@ void handleNewMessages(int numNewMessages) {
       String statusMessage = "Temperatura: " + String(temperature, 1) + " °C\n";
       statusMessage += "Humedad: " + String(humedad, 1) + " %\n";
       statusMessage += "DPV: " + String(DPV, 1) + " kPa\n";
-      statusMessage += "PH: " + String(PHval, 2);
-      statusMessage += "(" + String(PHvolt, 2) + " V)\n";
-      statusMessage += "Temp Agua: " + String(temperatureC, 2) + " °C\n";
+      //statusMessage += "PH: " + String(PHval, 2);
+      //statusMessage += "(" + String(PHvolt, 2) + " V)\n";
+      //statusMessage += "Sensor HS: " + String(sensorValue);
+//      statusMessage += "Humedad Suelo: " + String(humedadS) + " % (" + String(sensorValue) + ")\n";
+
       statusMessage += dateTime;  // Agrega la fecha y hora al mensaje
       bot.sendMessage(chat_id, statusMessage, "");
     }
@@ -809,3 +1026,100 @@ void handleNewMessages(int numNewMessages) {
     delay(500);
   }
 }
+
+
+/*const int NUM_COMMANDS = 70; // Cambia este número según tus comandos
+String commands[NUM_COMMANDS] = {
+  "/start",//1
+  "/config",//2
+  "/manual",//3
+  "/auto",//4
+  "/status", //4
+  "/infoconfig",//5 
+  "/DiasRiego", //6
+  "/resetDruidaBot",//7 
+  "/enviarData",//8
+  "/R1on", //9
+  "/R1off", //10
+  "/R1onTime", //11
+  "/R2on", //12
+  "/R2off", //13
+  "/R2onTime", //14
+  "/R2iron", //15
+  "/R2iroff", //16
+  "/R2ironTime",//17
+  "/R3on", //18
+  "/R3off", //19
+  "/R3onTime", //20
+  "/R4on", //21
+  "/R4off", //22
+  "/R4onTime", //23
+  "/controlRemoto",//24
+  "/R1auto", //25
+  "/R1timer", //26
+  "/R2auto", //27
+  "/R2irauto", //28
+  "/R3auto", //29
+  "/R3autoParam",//30 
+  "/R4auto",//31
+  "/minR1config",//32 
+  "/maxR1config", //33
+  "/paramR1config", //34
+  "/horaOnR1config", //35
+  "/minOnR1config", //36
+  "/horaOffR1config", //37
+  "/minOffR1config",//38
+  "/minR2config", //39
+  "/maxR2config", //40
+  "/paramR2config", //41
+  "/minR2irconfig", //42
+  "/maxR2irconfig", //43
+  "/paramR2irconfig",//44
+  "/horaOnR3config", //45
+  "/minOnR3config", //46
+  "/horaOffR3config", //47
+  "/minOffR3config", //48
+  "/minR3config", //49
+  "/maxR3config",//50
+  "/horaOnR4config", //51
+  "/minOnR4config", //52
+  "/horaOffR4config", //53
+  "/minOffR4config",//54
+  "/DiasRiegoInfo", //55
+  "/LunesRiego", //56
+  "/LunesNoRiego", //57
+  "/MartesRiego", //58
+  "/MartesNoRiego", //59
+  "/MiercolesRiego", //60
+  "/MiercolesNoRiego",//61
+  "/JuevesRiego", //62
+  "/JuevesNoRiego", //63
+  "/ViernesRiego", //64
+  "/ViernesNoRiego", //65
+  "/SabadoRiego", //66
+  "/SabadoNoRiego", //67
+  "/DomingoRiego", //68
+  "/DomingoNoRiego"//69
+};
+
+int commandCodes[NUM_COMMANDS] = {
+  1, 2, 3, 4, 5, 6, 7, 8, 9,
+  10, 11, 12, 13, 14, 15, 16, 17, 18,
+  19, 20, 21, 22, 23, 24, 25,
+  26, 27, 28, 29, 30, 31, 32,
+  33, 34, 35, 36, 37, 38, 39,
+  40, 41, 42, 43, 44, 45,
+  46, 47, 48, 49, 50, 51,
+  52, 53, 54, 55,
+  56, 57, 58, 59, 60, 61, 62,
+  63, 64, 65, 66, 67, 68, 69, 70
+};
+
+int getCommandCode(const String& command) {
+  for (int i = 0; i < NUM_COMMANDS; i++) {
+    if (command == commands[i]) {
+      return commandCodes[i];
+    }
+  }
+  return -1; // Comando no reconocido
+}*/
