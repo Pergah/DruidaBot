9e29462e22e17c168840e807399b7216cab18847
Pergah <119632014+Pergah@users.noreply.github.com>
2024-10-19 10:07:24 -0300

    Update config.h

--- config.h
+++ config.h
@@ -1,42 +1,6 @@
 
-/* Druida BOT 1.619
-
-El programa es capaz de controlar 4 reles independientes. 
-Pueden funcionar en modo Manual, o Automatico.
-Rele 1: Sube parametro (Hum, Temp, DPV, TempA)
-Rele 2: Baja parametro (Hum, Temp, DPV, TempA) 
-Rele 3: Timer diario (Ej: Luz) (Hora encendido / Hora Apagado)
-Rele 4: Timer diario + semanal (Ej: Riego) (Hora Enc / Hora Apag) + (DiaRiego / DiaNoRiego)
-
-Conexion de sensores: 
-
-Sensor Emisor IR -> 4
-Temperatura de Agua -> 5
-Sensor IR Receptor -> 14
-Temperatura y Humedad ambiente -> 19
-Sensor PH -> 34
-
-Chenge Log:
-
-* La red WiFi se puede cambiar desde el serial
-* El chat ID se puede cambiar desde el serial
-* Solucionado, no guardaba bien horarios R3 y R4
-* Se agrego la funcion "/resetDruidaBot" para reiniciar a distancia el dispositivo
-* Se mejoro sistema anti caida de internet (antes se bugeaba al caerse el internet)
-* Envia datos a una hoja de calculo de google
-* Se agrego la funcion de medir PH
-* Se agrego funcion de Medir temperatura de Agua
-* Se agrego funcion para enviar señal IR en el R2, se cargan los valores manualmente.
-* Se optimizo la parte del codigo donde se encienden y apagan los Rele.
-* Se agrego la funcion de "clonar" la señal IR con un sensor Receptor. (carga automatica)
-* Se arreglo la logica de R3 y R4, fallaba cuando HoraEnc > HoraApag
-* Se agrego la funcion de encender los reles por X segundos en modo Manual (/R1onTime)
-* Se agrego un nuevo Rele, llamado "Rele 2 IR", en vez de encender un Rele, manda una señal por infrarojo. (Especial, aire acondicionado).
-* Falta modificar logica del R3 para que pueda accionar o no, en funcion del sensor de HumedadSuelo
-*/
 
 #include "esp_system.h"
-//#include <DHT.h>
 #include <Wire.h>
 #include <RTClib.h>
 #include <WiFi.h>
@@ -46,10 +10,7 @@ Chenge Log:
 #include <UniversalTelegramBot.h>
 #include <EEPROM.h>
 #include <Time.h>
-//#include <TimeAlarms.h>
 #include <HTTPClient.h>
-//#include <OneWire.h>
-//#include <DallasTemperature.h>
 #include <Arduino.h>
 #include <IRrecv.h>
 #include <IRremoteESP8266.h>
@@ -57,7 +18,6 @@ Chenge Log:
 #include <IRsend.h>
 #include <Adafruit_AHTX0.h>
 #include <WebServer.h>
-//#include <HardwareSerial.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
@@ -76,9 +36,7 @@ Chenge Log:
 
 // Aca se muestra como van conectados los componentes
 
-//#define sensorTempAgua 5  //Sensor DS
 #define sensorIRpin 32    //Sensor Emisor IR
-//#define sensorHS 2 //humedad Suelo
 #define sensorIRreceptor 33
 #define RELAY4 19
 #define RELAY3 5
